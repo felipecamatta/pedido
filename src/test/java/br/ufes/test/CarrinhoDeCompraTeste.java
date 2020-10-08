090a456938529d1065e04f3a736241d573b32b82
said-gabriel <said-gabriel>
2020-10-08 17:35:44 -0300

    CT001 e CT005 apresentam a mesma saída na comparação mas mesmo assim diz que é diferente, CT004 requer revisao do grupo sobre o calculo do valorTotal do carrinho

--- src/test/java/br/ufes/test/CarrinhoDeCompraTeste.java
+++ src/test/java/br/ufes/test/CarrinhoDeCompraTeste.java
@@ -58,12 +58,12 @@ public class CarrinhoDeCompraTeste {
         //SIMULANDO A CRIACAO DE 2 PRODUTOS
         String nome1 = "smartphone", nome2 = "cafeteira", tipo1 = "Eletrônico", tipo2 = "Eletrodoméstico";
         double valor1 = 200.00, valor2 = 100.00, desc1 = 0.1, desc2 = 0.2;
-        double qtd1 = 200, qtd2 = 300;
+        double qtdEstoque1 = 200, qtdEstoque2 = 300;
         int qtdAdd1 = 5, qtdAdd2 = 10;
 
-        Produto p1 = new Produto(nome1, valor1, qtd1,
+        Produto p1 = new Produto(nome1, valor1, qtdEstoque1,
                 new TipoProduto(desc1, tipo1));
-        Produto p2 = new Produto(nome2, valor2, qtd2,
+        Produto p2 = new Produto(nome2, valor2, qtdEstoque2,
                 new TipoProduto(desc2, tipo2));
 
         //CRIANDO UMA LISTA DE ITENS E ADICIONANDO OS 2 PRODUTOS
@@ -74,18 +74,13 @@ public class CarrinhoDeCompraTeste {
                 new Item(p2, qtdAdd2));
 
         //SIMULANDO A ENTRADA DOS 2 ITENS NO CARRINHO
-        CarrinhoDeCompra carrinho
-                = new CarrinhoDeCompra(
-                        new Cliente("Carlos", "1552"),
-                        new Produto("smartphone", 200.00, 200,
-                        new TipoProduto(0.1, "Eletrônico")), 5, dt1);
-        carrinho.addItem(
-                new Produto("cafeteira", 100.00, 300,
-                        new TipoProduto(0.2, "Eletrodoméstico")), 10);
+        CarrinhoDeCompra carrinho = new CarrinhoDeCompra(new Cliente("Carlos", "1552"),p1, 5, dt1);
+        carrinho.addItem(p2, 10);
 
         //CONFERINDO SE OS 2 PRODUTOS CRIADOS EQUIVALEM AOS PRODUTOS DO CARRINHO
-        assertEquals(carrinho.getItens().get(1), itens.get(1));
+        //assertEquals(carrinho.getItens().get(1), itens.get(1));// 
         //assertEquals(carrinho.getItens().get(2), itens.get(2));
+        assertEquals(carrinho.getItens(),itens);
         
     }
 
@@ -99,30 +94,22 @@ public class CarrinhoDeCompraTeste {
         //SIMULANDO A CRIACAO DE 2 PRODUTOS
         String nome1 = "smartphone", nome2 = "cafeteira", tipo1 = "Eletrônico", tipo2 = "Eletrodoméstico";
         double valor1 = 200.00, valor2 = 100.00, desc1 = 0.01, desc2 = 0.02;
-        double qtd1 = 200, qtd2 = 300;
+        double qtdEstoque1 = 200, qtdEstoque2 = 300;
         int qtdAdd1 = 5, qtdAdd2 = 10;
 
-        Produto p1 = new Produto(nome1, valor1, qtd1,
+        Produto p1 = new Produto(nome1, valor1, qtdEstoque1,
                 new TipoProduto(desc1, tipo1));
-        Produto p2 = new Produto(nome2, valor2, qtd2,
+        Produto p2 = new Produto(nome2, valor2, qtdEstoque2,
                 new TipoProduto(desc2, tipo2));
 
         //CRIANDO UMA LISTA DE ITENS E ADICIONANDO OS 2 PRODUTOS
         ArrayList<Item> itens = new ArrayList<>();
-        itens.add(
-                new Item(p2, qtdAdd1));
-        itens.add(
-                new Item(p1, qtdAdd2));
+        itens.add(new Item(p2, qtdAdd1));
+        itens.add(new Item(p1, qtdAdd2));
 
         //SIMULANDO A ENTRADA DOS 2 ITENS NO CARRINHO
-        CarrinhoDeCompra carrinho
-                = new CarrinhoDeCompra(
-                        new Cliente("Carlos", "1552"),
-                        new Produto("smartphone", 200.00, 200,
-                                new TipoProduto(0.01, "Eletrônico")), 5, dt1);
-        carrinho.addItem(
-                new Produto("cafeteira", 100.00, 300,
-                        new TipoProduto(0.02, "Eletrodoméstico")), 10);
+        CarrinhoDeCompra carrinho = new CarrinhoDeCompra(new Cliente("Carlos", "1552"),p1, 5, dt1);
+        carrinho.addItem(p2, 10);
 
         //CONFERINDO SE O ITEM BUSCADO POR NOME ESTÁ FUNCIONANDO
         //assertEquals(carrinho.get,itens); //felipe olhar
@@ -199,36 +186,27 @@ public class CarrinhoDeCompraTeste {
         //SIMULANDO A CRIACAO DE 2 PRODUTOS
         String nome1 = "smartphone", nome2 = "cafeteira", tipo1 = "Eletrônico", tipo2 = "Eletrodoméstico";
         double valor1 = 200.00, valor2 = 100.00, desc1 = 0.01, desc2 = 0.02;
-        double qtd1 = 200, qtd2 = 300;
+        double qtdEstoque1 = 200, qtdEstoque2 = 300;
         int qtdAdd1 = 5, qtdAdd2 = 10;
 
-        Produto p1 = new Produto(nome1, valor1, qtd1,
+        Produto p1 = new Produto(nome1, valor1, qtdEstoque1,
                 new TipoProduto(desc1, tipo1));
-        Produto p2 = new Produto(nome2, valor2, qtd2,
+        Produto p2 = new Produto(nome2, valor2, qtdEstoque2,
                 new TipoProduto(desc2, tipo2));
 
         //CRIANDO UMA LISTA DE ITENS E ADICIONANDO OS 2 PRODUTOS
         ArrayList<Item> itens = new ArrayList<>();
-        itens.add(
-                new Item(p1, qtdAdd1));
-        itens.add(
-                new Item(p2, qtdAdd2));
+        itens.add(new Item(p1, qtdAdd1));
 
         //SIMULANDO A ENTRADA DOS 2 ITENS NO CARRINHO
-        CarrinhoDeCompra carrinho
-                = new CarrinhoDeCompra(
-                        new Cliente("Carlos", "1552"),
-                        new Produto("smartphone", 200.00, 200,
-                                new TipoProduto(0.01, "Eletrônico")), 5, dt1);
-        carrinho.addItem(
-                new Produto("cafeteira", 100.00, 300,
-                        new TipoProduto(0.02, "Eletrodoméstico")), 10);
+         CarrinhoDeCompra carrinho= new CarrinhoDeCompra(new Cliente("Carlos", "1552"),p1, qtdAdd1, dt1); //ADICIONANDO NO CARRINHO
+        carrinho.addItem(p2, qtdAdd2);
 
         //REMOVENDO A "cafeteira"
         carrinho.removerItem(p2);
 
         //CONFERINDO SE A REMOÇÃO FOI CONCLUÍDA CORRETAMENTE
-        assertEquals(carrinho.getItens(), new Item(p1, qtdAdd1));
+        assertEquals(carrinho.getItens(), itens);
     }
 
 }
